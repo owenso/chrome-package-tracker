50d77ad582de9f06164b6a52302580ca5aac796a
Owens O'Brien <owensobrien@gmail.com>
2015-11-23 01:51:18 -0500

    convert packpin response into object, set up synced storage

--- js/app.js
+++ js/app.js
@@ -1,36 +1,54 @@
-window.onload = function (){
-	$('#tracker-submit').click(takeTracking);
+window.onload = function() {
+    $('#tracker-submit').click(takeTracking);
 };
-var code;
 var mydata = JSON.parse(data);
 
-var takeTracking = function(){
-	code = $('#tracking-num').val();
-	console.log($('#tracking-num').val());
-	$('#tracking-num').val('');
-
-
-	var request = new XMLHttpRequest();
-
-request.open('POST', 'https://api.packpin.com/v2/carriers/detect');
-
-request.setRequestHeader('packpin-api-key', mydata.packPinKey);
-request.setRequestHeader('Content-Type', 'application/json');
-
-
-request.onreadystatechange = function () {
-  if (this.readyState === 4) {
-    console.log('Status:', this.status);
-    console.log('Headers:', this.getAllResponseHeaders());
-    console.log('Body:', this.responseText);
-  }
+var takeTracking = function() {
+    var code = $('#tracking-num').val();
+    var title = $('#tracking-title').val();
+    $('#tracking-num').val('');
+    $('#tracking-title').val('');
+    makeRequest(code, title);
 };
 
-var body = {
-  'code': '9361289949018104956352'
-};
 
-request.send(JSON.stringify(body));
+var makeRequest = function(trackingNumber, title) {
 
+    var request = new XMLHttpRequest();
+
+    request.open('GET', 'https://api.packpin.com/v2/trackings');
+
+    request.setRequestHeader('packpin-api-key', mydata.packPinKey);
+    request.setRequestHeader('Content-Type', 'application/json');
+
+
+    request.onreadystatechange = function() {
+        if (this.readyState === 4) {
+            console.log('Status:', this.status);
+            console.log('Headers:', this.getAllResponseHeaders());
+            console.log('Body:', this.responseText);
+            var jsonified = JSON.parse(this.responseText);
+            var packageObject = {
+                title: title,
+                status: jsonified.body.items[0].status,
+                est_del: jsonified.body.items[0].estimated_delivery,
+                details: jsonified.body.items[0].track_details,
+                carrier: jsonified.body.items[0].carrier_code,
+                dest_zip: jsonified.body.items[0].destination_zip,
+            };
+            
+            chrome.storage.sync.set({
+                trackingNumber: packageObject
+            }, function() {
+                console.log('saved to synced storage');
+            });
+        }
+    };
+
+    var body = {
+        'code': trackingNumber
+    };
+
+    request.send(JSON.stringify(body));
 
 };
