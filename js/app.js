3eb5770269884485e059f616ec007f49adcf78ed
Owens O'Brien <owensobrien@gmail.com>
2015-11-26 00:51:01 -0500

    fix request to API to act as create route  and added index API call

--- js/app.js
+++ js/app.js
@@ -13,10 +13,10 @@ var takeTracking = function() {
 
 
 var makeRequest = function(trackingNumber, title) {
-
+    console.log(trackingNumber);
     var request = new XMLHttpRequest();
 
-    request.open('GET', 'https://api.packpin.com/v2/trackings');
+    request.open('POST', 'https://api.packpin.com/v2/trackings');
 
     request.setRequestHeader('packpin-api-key', mydata.packPinKey);
     request.setRequestHeader('Content-Type', 'application/json');
@@ -27,28 +27,56 @@ var makeRequest = function(trackingNumber, title) {
             console.log('Status:', this.status);
             console.log('Headers:', this.getAllResponseHeaders());
             console.log('Body:', this.responseText);
-            var jsonified = JSON.parse(this.responseText);
-            var packageObject = {
-                title: title,
-                status: jsonified.body.items[0].status,
-                est_del: jsonified.body.items[0].estimated_delivery,
-                details: jsonified.body.items[0].track_details,
-                carrier: jsonified.body.items[0].carrier_code,
-                dest_zip: jsonified.body.items[0].destination_zip,
-            };
-            
-            chrome.storage.sync.set({
-                trackingNumber: packageObject
-            }, function() {
-                console.log('saved to synced storage');
-            });
+            console.log(typeof(this.responseText));
+            // var jsonified = JSON.parse(this.responseText);
+            // var packageObject = {
+            //     title: title,
+            //     status: jsonified.body.items[0].status,
+            //     est_del: jsonified.body.items[0].estimated_delivery,
+            //     details: jsonified.body.items[0].track_details,
+            //     carrier: jsonified.body.items[0].carrier_code,
+            //     dest_zip: jsonified.body.items[0].destination_zip,
+            // };
+
+            // chrome.storage.sync.set({
+            //     trackingNumber: packageObject
+            // }, function() {
+            //     console.log('saved to synced storage');
+            // });
         }
     };
 
     var body = {
-        'code': trackingNumber
+        'code': trackingNumber,
+        'description': title
     };
 
     request.send(JSON.stringify(body));
 
+};
+
+var fillPage = function() {
+
+    var request = new XMLHttpRequest();
+
+    request.open('GET', 'https://api.packpin.com/v2/trackings');
+
+    request.setRequestHeader('packpin-api-key', mydata.packPinKey);
+    request.setRequestHeader('Content-Type', 'application/json');
+
+    request.onreadystatechange = function() {
+        if (this.readyState === 4) {
+            console.log('Status:', this.status);
+            console.log('Headers:', this.getAllResponseHeaders());
+            console.log('Body:', this.responseText);
+			     	var jsonified = JSON.parse(this.responseText);
+
+			     	for (var i=0; i<jsonified.body.items.length; i++){
+			     		console.log('running x times:');
+			     		console.log(i);
+			      	$('#package-table tr:last').after('<tr><td>'+jsonified.body.items[i].description+'</td><td>'+jsonified.body.items[i].status+'</td><td>' + jsonified.body.items[i].estimated_delivery+'</td>');
+			     	}
+        }
+    };
+    request.send();
 };
